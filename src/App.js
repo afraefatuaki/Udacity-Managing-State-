3b3c8e5da0e0234611537371f8e2ff5951f47ee1
afraefatuaki <afrae@elfatuaki.com>
2021-08-16 13:29:25 +0000

    managing state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
 
 class App extends Component {
+  state = {
+    correctAnswer: 0,
+    numQuestions: 0,
+  };
+   
+  handleAnswer = answerWasCorrect =>{
+  if (answerWasCorrect){
+  this.setState(currState=>({
+	correctAnswe: currState.correctAnswer + 1,  
+  }));
+  }
+  this.setState(currState => ({
+  	numQuestions: currState.numQuestions + 1,
+  }))
+  }
   render() {
     return (
       <div className="App">
--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,29 @@
 
 
 class Game extends React.Component{
+  constructor(props){
+  super(props)
+    this.state={
+      value1:valuesArray[0],
+      value2:valuesArray[1],
+      value3:valuesArray[2],
+      proposedAnswer: valuesArray[3],
+      
+    };
+  
+  }
+  makeNewQuestion=()=>{
+	value1 = Math.floor(Math.random() * 100);
+	value2 = Math.floor(Math.random() * 100);
+	value3 = Math.floor(Math.random() * 100);
+	proposedAnswer = Math.floor(Math.random() * 3) +( value1 + value2 + value3); 
+    return[value1,value2,value3,proposedAnswer];
+  }
+  
+  updateState = newValuesArray=>{
+ 	this.setState(currState=>({value1 = Math.floor(Math.random() * 100);
+	value2 = Math.floor(Math.random() * 100);
+	value3 = Math.floor(Math.random() * 100);
+	proposedAnswer = Math.floor(Math.random() * 3) +( value1 + value2 + value3)}))
+  }
 }
